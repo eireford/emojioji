d32e1828c2b213e8ddfef7edba19902e8da09829
Eire Ford <eireford@gmail.com>
2020-10-30 09:04:37 -0700

    add emoji-list component as generated by cli

--- src/app/emojioji/emoji/emoji.module.ts
+++ src/app/emojioji/emoji/emoji.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmojiRoutingModule } from './emoji-routing.module';
+import { EmojiListComponent } from './emoji-list/emoji-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [EmojiListComponent],
   imports: [
     CommonModule,
     EmojiRoutingModule
