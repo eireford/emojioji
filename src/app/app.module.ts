4109f82bf226334d4022b024fb84403342309469
Eire Ford <eireford@gmail.com>
2020-10-30 09:27:40 -0700

    add page-not-found component as generated by cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmojiojiModule } from './emojioji/emojioji.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
