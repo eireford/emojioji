43ccb6081d07728774abd9f37a93ed9faebe62f1
Eire Ford <eireford@gmail.com>
2020-11-10 22:46:42 -0800

    add firebase storage service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { TranslateModule } from '@ngx-translate/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule, BUCKET } from '@angular/fire/storage';
 
 import { environment } from '@env/environment';
 import { CoreModule } from '@core';
@@ -25,6 +27,8 @@ import { AppRoutingModule } from './app-routing.module';
     TranslateModule.forRoot(),
     BrowserAnimationsModule,
     MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule,
     CoreModule,
     SharedModule,
     ShellModule,
@@ -33,7 +37,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule, // must be imported as the last module as it contains the fallback route
   ],
   declarations: [AppComponent],
-  providers: [],
+  providers: [{ provide: BUCKET, useValue: 'emojioji.appspot.com' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
